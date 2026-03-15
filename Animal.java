f96338696f48c8945be42a4e83a5353b507702b2
Moiz Mustafa <k253040@nu.edu.pk>
2026-03-15 14:21:13 +0500

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,41 @@
+package Lab07_Tasks;
+
+class Animal {
+    Animal(){}
+
+    public void makeSound() {
+        System.out.println("Animal Makes Sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Dog Barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Cat Meows");
+    }
+}
+
+class Cow extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Cow Moos");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        Animal a3 = new Cow();
+        a1.makeSound();
+        a2.makeSound();
+        a3.makeSound();
+    }
+}
